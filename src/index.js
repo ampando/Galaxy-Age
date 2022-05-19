334f4cbe8af1acd4a0a5ccd328d29f3061901ab7
Adrienne <100542795+ampando@users.noreply.github.com>
2022-05-19 09:02:50 -0700

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,27 @@
+import $ from 'jquery';
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './css/styles.css';
+import Template from './template.js'; //change name 
+
+$(document).ready(function() {
+  $('#triangle-checker-form').submit(function(event) {
+    event.preventDefault();
+    const length1 = parseInt($('#length1').val());
+    const length2 = parseInt($('#length2').val());
+    const length3 = parseInt($('#length3').val());
+    const triangle = new Triangle(length1, length2, length3);
+    const response = triangle.checkType();
+    $('#response').append(`<p>${response}</p>`);
+  });
+
+  $('#rectangle-area-form').submit(function(event) {
+    event.preventDefault();
+    const length1 = parseInt($('#rect-length1').val());
+    const length2 = parseInt($('#rect-length2').val());
+    const rectangle = new Rectangle(length1, length2);
+    const response = rectangle.getArea();
+    $('#response2').append(`<p> The area of the rectangle is ${response}.</p>`);
+  });
+});
+
---
+++ src/js/rectangle.js
@@ -0,0 +1,8 @@
+
+export default class Rectangle {
+  constructor() {
+  }
+getArea() {
+  return this.side1 * this.side2;
+}
+}
